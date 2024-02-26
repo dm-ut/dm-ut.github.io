59ad43f03d69f33ee3cbdcb30c4a031d143274a3
VavRe <vahyd@live.com>
2024-02-26 15:31:20 +0330

    fully responsive and functional version

--- main.js
+++ main.js
@@ -3,4 +3,23 @@ import Alpine from 'alpinejs'
  
 window.Alpine = Alpine
  
+
+
+const header = document.getElementById('header');
+let prevScroll = window.scrollY;
+window.onscroll = () => {
+  const currentScroll = window.scrollY;
+  if (prevScroll > currentScroll) {
+    console.log("up main")
+    header.classList.remove('-translate-y-full');
+
+  } else {
+    header.classList.add('-translate-y-full');
+    console.log("down main")
+
+  }
+
+  prevScroll = currentScroll;
+}
+
 Alpine.start()
