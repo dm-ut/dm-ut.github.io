33e73c7fd24c0f4fd42192b8a3f5f5de4f2d9fce
Vahid <vahid@codecodecode.ir>
2025-02-17 18:11:55 +0330

    new jekyll version

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.JEKYLL_ENV == 'production'
+      ? [require('cssnano')({ preset: 'default' })]
+      : [])
+  ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+    content: [
+      './_includes/**/*.{html,md}',
+      './_layouts/**/*.{html,md}',
+      './_posts/**/*.{html,md}',
+      './_years/**/*.{html,md}',
+      './*.{html,md}',
+    ],
     theme: {
-    extend: {
-      colors:{
-        "primary": {
-          "50": "#EAEBF8",
-          "100": "#cfcfd5",
-          "200": "#9e9faa",
-          "300": "#6e7080",
-          "400": "#3d4055",
-          "500": "#0d102b",
-          "600": "#0a0d22",
-          "700": "#080a1a",
-          "800": "#050611",
-          "900": "#030309"
-        }      
+      extend: {
+        colors:{
+            "primary": {
+              "50": "#EAEBF8",
+              "100": "#cfcfd5",
+              "200": "#9e9faa",
+              "300": "#6e7080",
+              "400": "#3d4055",
+              "500": "#0d102b",
+              "600": "#0a0d22",
+              "700": "#080a1a",
+              "800": "#050611",
+              "900": "#030309"
+            }
+        },      },
     },
-  },
-  plugins: [],
-}
-
-}
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
+  
